0d500cc855c5c887ed59e337ad8be9baf747ebed
ljbrewer <ljbrewer.lb@gmail.com>
2021-07-14 12:12:05 -0700

    uploading Quiz files 2nd time

--- script.js
+++ script.js
@@ -61,47 +61,92 @@ var questions = [
   
 ];
 console.log(questions);
+var timerElement = document.querySelector(".timerContainer");
+var timerCount;
 
-function placeQuestionOnPage(question) {
-        console.log("place on page");
-        var {questiontext, answers} = question;
-        console.log("the question is" + question)
-        gameInfo.currentQuestion = question
-        var questionContainer = document.querySelector(".questionContainer");
-        var qText=document.createElement("h2");
-        qTest.textContent=questionText;
-        var answerList = document.createElement("ol");
-        var answerItems = [];
- 
-        for (let i=0; i<answers.length; i++){
-        console.log("in answer loop");
-        var answerItem = document.createElement("li");
-        answerItem.dataset.index = i
-        answerItem.textContent = answers[i];
-       
-        console.log(answerlist);
-
-        answerItems.push(answerItem);
-     
+function startTimer() {
+    // Sets timer
+    console.log("timer is started")
+    timer = setInterval(function () {
+        timerCount--;
+        timerElement.textContent = timerCount;
+        if (timerCount >= 0) {
+            // Tests if win condition is met
+            if (isWin && timerCount > 0) {
+                // Clears interval and stops timer
+                clearInterval(timer);
+                winGame();
+            }
+        }
+        // Tests if time has run out
+        if (timerCount === 0) {
+            // Clears interval
+            clearInterval(timer);
+            loseGame();
+        }
+    }, 1000);
+    return timerCount;
 }
-    answerList.append(answerItem);
-    questionContainer.append(qText);
+
+var gameInfo = {
+    currentQuestion:{}
+}
+   
+    function startGame() {
+   
+        console.log("startGame");
+
+        placeQuestionOnPage(questions[0]);
+        var timerCount = 10
+        startTimer();
+
+    }
+   
+    function placeQuestionOnPage(question) {
+
+        console.log("place on page");
+
+          //  var questiontext = question.questiontext;
+          //  var answers = question.answers;
+          //  var correctAnswerIndex = question.correctAnswerIndex
+            var {questiontext, answers} = question;
+
+            gameInfo.currentQuestion = question
+
+            var questionContainer = document.querySelector(".questionContainer");
+            var qText=document.createElement("h2");
+            qText.textContent=questiontext;
+            var answerList = document.createElement("ol");
+            var answerItems = [];
+       // console.log("the question is " + questiontext)
+    
+        for (let i=0; i<answers.length; i++){
+
+         //   console.log("in answer loop");
+            
+            var answerItem = document.createElement("li");
+            var answerItemBtn = document.createElement("button");
+
+            // data-index attr for each item
+            answerItem.dataset.index = i
+           // answerItem.textContent = answers[i];
+            answerItemBtn.textContent = answers[i];
+            answerItemBtn.dataset.index = i;
+            answerItem.append(answerItemBtn);
+            answerItems.push(answerItem);
+
+            console.log(answerItem);
+        }
+        // loop thru possible answers
+        for (let i = 0; i < answerItems.length; i++) {
+            answerList.append(answerItems[i]);  
+        }
+            questionContainer.append(qText, answerList );
   
 }  
 
 console.log("line 87");
 
 
-function startGame(event) {
-    event.preventDefault;
-
-    console.log("startGame");
-
-    for (let index = 0; index < questions.length; index++) {
-        const element = questions[index];
-       
-    }
-    placeQuestionOnPage()
-}
 
 document.querySelector("#startbutton").addEventListener("click", startGame)
